4cad89a9c7e39efb07658078797c19ca438631ec
Kyle Kolodziej <kkolodziej@smu.edu>
2020-02-03 18:31:33 -0600

    Code for PA00

---
+++ cmake-build-debug/CMakeFiles/PA00.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA00.dir/main.cpp.o"
+  "PA00.pdb"
+  "PA00"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
